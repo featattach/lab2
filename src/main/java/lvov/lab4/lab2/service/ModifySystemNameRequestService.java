81105e748e250efaecf8a97489be670eb8e84bb8
featattach <featattach@gmail.com>
2023-12-20 14:35:49 +0500

    first service 4 lab 2 task ended lab 4 finished

--- src/main/java/lvov/lab4/lab2/service/ModifySystemNameRequestService.java
+++ src/main/java/lvov/lab4/lab2/service/ModifySystemNameRequestService.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 @Service
-public class ModifySystemNameRequestService implements ModifyRequestService{
+public class ModifySystemNameRequestService implements ModifyRequestService {
     @Override
-    public void modify(Request request){
+    public void modify(Request request) {
         request.setSystemName("Service 1");
         request.setSource("Modified Source in Service 1");
 
