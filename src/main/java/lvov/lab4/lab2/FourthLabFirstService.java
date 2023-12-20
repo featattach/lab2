29ad91977de8ea64137f7d076b4f1ddeb762005d
featattach <featattach@gmail.com>
2023-12-20 15:12:17 +0500

    first service 4 lab 2 task ended lab 4 finished

--- src/main/java/lvov/lab4/lab2/FourthLabFirstService.java
+++ src/main/java/lvov/lab4/lab2/FourthLabFirstService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab4Application {
+public class FourthLabFirstService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab4Application.class, args);
+		SpringApplication.run(FourthLabFirstService.class, args);
 	}
 
 }
